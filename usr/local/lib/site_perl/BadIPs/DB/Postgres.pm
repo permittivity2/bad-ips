9943486a53d7d510034fe27a4c2ff00b3fb877dd
Gardner <gardner@homelab>
2026-05-15 19:50:44 -0500

    Release v3.5.5

--- usr/local/lib/site_perl/BadIPs/DB/Postgres.pm
+++ usr/local/lib/site_perl/BadIPs/DB/Postgres.pm
@@ -6,7 +6,7 @@ use DBI;
 use Log::Log4perl qw(get_logger);
 use Sys::Hostname;
 
-our $VERSION = '3.5.4';
+our $VERSION = '3.5.5';
 
 my $log = get_logger("BadIPs::DB::Postgres");
 
--- usr/local/lib/site_perl/BadIPs/NFT.pm
+++ usr/local/lib/site_perl/BadIPs/NFT.pm
@@ -5,7 +5,7 @@ use warnings;
 use JSON;
 use Log::Log4perl qw(get_logger);
 
-our $VERSION = '3.5.4';
+our $VERSION = '3.5.5';
 
 my $log = get_logger("BadIPs::NFT");
 
