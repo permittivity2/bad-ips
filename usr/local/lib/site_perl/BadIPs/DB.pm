11cd6d483ddfdf4850d012c9fc1f5f8d04f29d00
Gardner <gardner@homelab>
2026-06-05 20:35:19 -0500

    Release v3.5.22

--- usr/local/lib/site_perl/BadIPs/DB.pm
+++ usr/local/lib/site_perl/BadIPs/DB.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Log::Log4perl qw(get_logger);
 
-our $VERSION = '3.5.21';
+our $VERSION = '3.5.22';
 
 my $log = get_logger("BadIPs::DB");
 
--- usr/local/lib/site_perl/BadIPs/Plugins/GeneralLogMonitor.pm
+++ usr/local/lib/site_perl/BadIPs/Plugins/GeneralLogMonitor.pm
@@ -8,7 +8,7 @@ use Regexp::Common qw(net);  # Exports %RE{net}
 use Data::Dumper;
 use List::Util qw(any);
 use JSON qw(decode_json);
-our $VERSION = '3.5.21';
+our $VERSION = '3.5.22';
 
 my $log = get_logger();
 
